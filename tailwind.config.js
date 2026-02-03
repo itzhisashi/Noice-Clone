a8d2ce04bb8c01b3807c9566d21bb1d1e582af46
Hisashi 久 <80805078+itzhisashi@users.noreply.github.com>
2026-02-03 15:06:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ export default {
                 primary: "#0ea5e9", // Sky 500
                 secondary: "#6366f1", // Indigo 500
                 accent: "#10b981", // Emerald 500
-            }
+            },
+            backgroundImage: {
+        'hero': "linear-gradient(to right, rgba(37,99,235,0.85), rgba(67,56,202,0.85)), url('/file.webp')",
+      }
         },
     },
     plugins: [],
